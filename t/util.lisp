ef726d7fd311a9ad80d950797d955d172cecc6be
Matt Novenstern <fisxoj@gmail.com>
2020-01-11 00:38:42 -0500

    Fix broken test
    alexandria hit 1.0.0, congrats!

--- t/util.lisp
+++ t/util.lisp
@@ -7,11 +7,11 @@
 (in-package :coo-test.util)
 
 (deftest test-make-url
-  (ok (string= (make-url nil :alexandria "EMPTYP" :function) "alexandria.0.dev.html#function__emptyp")
+  (ok (string= (make-url nil :alexandria "EMPTYP" :function) "alexandria.1.0.0.html#function__emptyp")
       "Formats a url for a symbol in a non-common-lisp package.")
 
   (ok (string= (make-url nil :cl "LIST" :function) "http://www.lispworks.com/reference/HyperSpec/Body/a_list.htm")
       "Formats a url for a symbol in the common-lisp package (linking to the hyperspec.")
 
-  (ok (string= (make-url nil :alexandria) "alexandria.0.dev.html")
+  (ok (string= (make-url nil :alexandria) "alexandria.1.0.0.html")
       "Formats a url for a package."))
